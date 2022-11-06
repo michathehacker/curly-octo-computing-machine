0bde76ce9d7e8bde57e3119c550d2c9560be1cdb
michathehacker <michalis.jackson@faulkner.edu>
2022-11-06 09:16:53 -0600

    Latest changes with if statements added. Could not run  it "The project doesn't know how to run the project.."

--- UnitConvertion/UnitConvertion/Program.cs
+++ UnitConvertion/UnitConvertion/Program.cs
@@ -6,30 +6,46 @@ namespace UnitConvertion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Convertion of Kg to Grams");
-            Console.WriteLine("Enter the amount of Kg:");
-            double amountOfKG = Double.Parse(Console.ReadLine());
-            double amountOfGrams = amountOfKG * 1000;
-            Console.WriteLine( amountOfKG + "kg = " + amountOfGrams + "g");
 
-            Console.WriteLine("Convertion of Grams to Kg");
-            Console.WriteLine("Enter the amount of Grams:");
-            double Grams = Double.Parse(Console.ReadLine());
-            double Kgs = Grams / 1000;
-            Console.WriteLine(Grams + "g = " + Kgs + "kg");
+            
+            Console.WriteLine("1-Convertion of Kg to Grams");
+            Console.WriteLine("2-Convertion of Grams to Kg");
+            Console.WriteLine("3-Convertion of Meters to Miles");
+            Console.WriteLine("4-Convertion of Miles to Meters");
 
-            Console.WriteLine("Convertion of Meters to Miles");
-            Console.WriteLine("Enter the amount of Meters:");
-            double Meters = Double.Parse(Console.ReadLine());
-            double Miles = Meters / 1600;
-            Console.WriteLine(Meters + "m = " + Miles + "mi");
+            int choose = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Convertion of Miles to Meters");
-            Console.WriteLine("Enter the amount of Miles:");
-            double miles = Double.Parse(Console.ReadLine());
-            double meters = miles * 1600;
-            Console.WriteLine(miles + "mi = " + meters + "m");
+            if (choose == 1)
+            {
+                Console.WriteLine("Enter the amount of Kg:");
+                double amountOfKG = Double.Parse(Console.ReadLine());
+                double amountOfGrams = amountOfKG * 1000;
+                Console.WriteLine(amountOfKG + "kg = " + amountOfGrams + "g");
+            }
 
+            if (choose == 2)
+            {
+                Console.WriteLine("Enter the amount of Grams:");
+                double Grams = Double.Parse(Console.ReadLine());
+                double Kgs = Grams / 1000;
+                Console.WriteLine(Grams + "g = " + Kgs + "kg");
+            }
+
+            if (choose == 3)
+            {
+                Console.WriteLine("Enter the amount of Meters:");
+                double Meters = Double.Parse(Console.ReadLine());
+                double Miles = Meters / 1600;
+                Console.WriteLine(Meters + "m = " + Miles + "mi");
+            }
+
+            if (choose == 4)
+            {
+                Console.WriteLine("Enter the amount of Miles:");
+                double miles = Double.Parse(Console.ReadLine());
+                double meters = miles * 1600;
+                Console.WriteLine(miles + "mi = " + meters + "m");
+            }
 
 
         }
